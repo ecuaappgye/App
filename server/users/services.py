12f5afa5a503f77a59effac44d44c1d2c64d1b10
italo <italobarzola18@gmail.com>
2021-07-13 16:56:25 -0500

    Add password reset

--- server/users/services.py
+++ server/users/services.py
@@ -1,4 +1,6 @@
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from server.users.selectors import user_by_email, user_by_id
+from django.utils.encoding import smart_bytes
 from .models import BaseUser
 
 
@@ -36,5 +38,14 @@ def user_update_profile(*, user_id:int, data)->BaseUser:
 def user_password_reset(*, email:str)->BaseUser:
    user = user_by_email(email=email)
 
+   token = user_make_token(user_id=user.id)
+
+   # Avanzar la concatenación url
+
    return user
 
+
+def user_make_token(*, user_id:int):
+    user = urlsafe_base64_encode(smart_bytes(user_id))
+
+    return user
