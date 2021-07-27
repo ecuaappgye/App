04c4673cf6911b1039a1021a0e1b80f54803a766
italo <italobarzola18@gmail.com>
2021-07-27 08:42:45 -0500

    Add signal on presave token

--- config/settings/sms.py
+++ config/settings/sms.py
@@ -6,3 +6,5 @@ VERIFY_TOKEN_EXPIRE_TIME = 15 * 60
 
 # Mensaje cuando un token ha expirado.
 VERIFY_TOKEN_EXPIRED_MESSAGE = "Código de activación ha caducado."
+
+VERIFY_TOKEN_GENERATION_ATTEMPTS = 3
--- server/authentication/signals.py
+++ server/authentication/signals.py
@@ -1,9 +1,33 @@
+from django.core.exceptions import ValidationError
+from server.authentication.utils import generate_numeric_token
 from django.dispatch import receiver
 from django.db.models import signals
 from .models import CallbackToken
+from django.conf import settings
 
 
 @receiver(signals.pre_save, sender=CallbackToken)
 def check_unique_token(sender, instance, **kwargs):
     if instance._state.adding:
-        # pass hoy
+        unique = False
+        if not isinstance(instance, CallbackToken):
+            return None
+        
+        tokens = CallbackToken.objects.filter(key=instance.key, is_active=True).exists()
+        tries = 0
+        if tokens:
+            while tries < settings.VERIFY_TOKEN_GENERATION_ATTEMPTS:
+                tries = tries+1
+                refresh_key = generate_numeric_token()
+                instance.key = refresh_key
+
+                if not CallbackToken.objects.filter(key=instance.key, is_active=True).exists():
+                    unique = True
+                    break
+            
+            if not unique:
+                raise ValidationError('El token no pudo ser creado como único.')
+
+
+
+
